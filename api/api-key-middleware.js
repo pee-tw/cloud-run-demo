f0ced95899660dc4df6e8023ca4519829a9ab1e3
Pee Tankulrat <{ID}+{username}@users.noreply.github.com>
2022-10-18 15:21:36 +0700

    Set frontend url for CORS

--- api/api-key-middleware.js
+++ api/api-key-middleware.js
@@ -1,11 +1,9 @@
-const FRONTEND_URL = 'https://frontend-5crg7qpwtq-as.a.run.app'
-
 module.exports = (req, res, next) => {
   const secret = process.env.API_SECRET
   const apiKey = req.get('x-api-key')
   if (apiKey !== secret) return res.sendStatus(401)
 
-  res.header('Access-Control-Allow-Origin', FRONTEND_URL)
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL)
   res.header('Access-Control-Allow-Headers', '*')
 
   next()
